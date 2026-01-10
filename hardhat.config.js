77dfbef74ea0335d629faad6b6cd210bb7557a9f
Ignacia Heyer <ignacia.heyer@ignaciaheyer.com>
2026-01-10 18:26:49 -0500

    Configure Hardhat for Sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY || "";
+const AGENT_PRIVATE_KEY = process.env.AGENT_PRIVATE_KEY || "";
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [OWNER_PRIVATE_KEY, AGENT_PRIVATE_KEY].filter(Boolean),
+    },
+  },
+};
 
